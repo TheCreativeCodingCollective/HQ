fee0bdd821a86eba42942c3499933194a16683f2
Shers <62606407+shers003@users.noreply.github.com>
2021-03-19 16:41:28 +0000

    Fixed path error added comments

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,29 @@
+/***** Dependencies ********/
 const express = require('express');
 const env = require('dotenv').config();
+const path = require('path');
 
+/******* Global Variables *********/
 const app = express();
 const PORT = process.env.PORT;
-const frontend = '../frontend/'
+const frontend = __dirname + '/../frontend/'
 
+/******** Middleware **********/
 app.use(express.static('../frontend'))
 
-app.get('/', (req, res)=>{
+
+/******** Routing *******/
+app.route('/')
+.get((req, res)=>{
   res.sendFile(frontend + 'index.html');
 });
 
-app.get('/gallery', (req, res)=>{
-  //this path doesn't work not sure why gonna solve
-  res.sendFile(frontend + 'gallery.html');
+app.route('/gallery')
+.get((req, res)=>{
+  res.sendFile( path.resolve(frontend + 'gallery.html') );
 });
 
+/****** Listening on port ******/
 app.listen(PORT, ()=>{
   console.log('Listening on ' + PORT);
 });
